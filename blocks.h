1e5562fc1faea021c7555b5a6844795a20a368a6
Manfred Mjka <manfred.mjka@sclable.com>
2021-08-25 14:18:48 +0200

    feat: add load

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/													/*Update Interval*/	/*Update Signal*/
 	{" ", 		"~/.local/bin/dwmblocks/memory",		30,									0},
+	{" ", 		"~/.local/bin/dwmblocks/load",			30,									0},
 	{" ", 		"~/.local/bin/dwmblocks/battery",		15,									0},
 	{"", 			"date '+%a %d.%b %y %H:%M'",				30,									0},
 };
