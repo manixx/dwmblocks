b5f2ace4a3400c856247c7ef03543c03b73addf0
Manfred Mjka <manixx90@gmail.com>
2022-06-06 13:31:52 +0200

    chore: add xautolock indicator

--- blocks.h
+++ blocks.h
@@ -1,11 +1,12 @@
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/													/*Update Interval*/	/*Update Signal*/
-	{" ", 		"~/.local/bin/governor",						30,									0},
-	{" ", 		"~/.local/bin/dwmblocks/temp",			30,									0},
-	{" ", 		"~/.local/bin/dwmblocks/memory",		30,									0},
-	{" ", 		"~/.local/bin/dwmblocks/load",			30,									0},
-	{" ", 		"~/.local/bin/dwmblocks/battery",		15,									0},
-	{"", 			"date '+%a %d.%b %y %H:%M'",				30,									0},
+	/*Icon*/	/*Command*/																						/*Update Interval*/	/*Update Signal*/
+	{"", 			"pgrep > /dev/null -x xautolock && echo  || echo  ",	10,									0},
+	{" ", 		"~/.local/bin/governor",																30,									0},
+	{" ", 		"~/.local/bin/dwmblocks/temp",													30,									0},
+	{" ", 		"~/.local/bin/dwmblocks/memory",												30,									0},
+	{" ", 		"~/.local/bin/dwmblocks/load",													30,									0},
+	{" ", 		"~/.local/bin/dwmblocks/battery",												15,									0},
+	{"", 			"date '+%a %d.%b %y %H:%M'",														30,									0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
