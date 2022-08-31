f500edf943f619c394f80f3f28bf1cb6f77941e6
Manfred Mjka <manixx90@gmail.com>
2022-08-31 13:56:49 +0200

    chore: add keymap

--- blocks.h
+++ blocks.h
@@ -1,10 +1,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/																						  /*Update Interval*/	/*Update Signal*/
-	{"", 			"pgrep > /dev/null -x xautolock && echo  || echo  ",	10,									0},
-	{" ", 		"dwmblocks-memory",																			30,									0},
-	{" ",		"dwmblocks-load",																				30,									0},
-	{" ", 		"dwmblocks-battery",																		15,									0},
-	{"", 			"date '+%a %d.%b %y %H:%M'",														30,									0},
+	{"", 			"pgrep > /dev/null -x xautolock && echo  || echo  ",				10,									0},
+	{" ", 		"setxkbmap -query | grep layout | tr -s ' ' | cut -d' ' -f2",	5,									0},
+	{" ", 		"dwmblocks-memory",																						30,									0},
+	{" ",		"dwmblocks-load",																							30,									0},
+	{" ", 		"dwmblocks-battery",																					15,									0},
+	{"", 			"date '+%a %d.%b %y %H:%M'",																	30,									0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
