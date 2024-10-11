362958cc14ae40c323e536556e647c1872115d12
Manfred Krieger <manixx90@gmail.com>
2024-10-11 22:34:22 +0200

    minior adaptions

--- blocks.h
+++ blocks.h
@@ -2,7 +2,7 @@ static const Block blocks[] = {
 	{" ", "dwmblocks-memory",                                    30, 0},
 	{" ", "dwmblocks-load",                                      30, 0},
 	{" ", "dwmblocks-battery",                                   15, 0},
-	{"",   "date '+%a %d.%b %y %H:%M'",                           30, 0},
+	{"",   "date '+%a %d.%b (%m) %y %H:%M'",                      30, 0},
 };
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
